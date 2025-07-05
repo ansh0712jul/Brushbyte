52674083ccfff4558d9ecc39771592c582c14e52
Ansh agrawal <agrawalansh624@gmail.com>
2025-07-05 16:51:16 +0530

    setup frontend with shadcn ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "./components/ui/button.jsx"
+
+const App = () => {
+  return (
+    <>
+    <h1 className='bg-amber-400'>hii this side react with tailwind and shadcn .</h1>
+    <Button >press me !</Button>
+    </>
+  )
+}
+
+export default App
